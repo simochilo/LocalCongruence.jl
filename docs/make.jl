1aa4b94165e87e53299122f8842729552222d02f
Elia Onofri <30669145+eOnofri04@users.noreply.github.com>
2020-03-31 15:14:38 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,19 @@ makedocs(
 	assets = ["assets/LocalCongruences.css", "assets/logo.png"],
 	pages = [
 		"1 - Home" => "index.md",
+		"2 - Getting Started" => "start.md",
+		"3 - Complexes and Chain Operators" => "theory.md",
+		"4 - Graph Blas Introduction" => "graph_blas.md",
+		"Implementations" => [
+			"5.1 - Array of Arrays" => "aa_implementation.md",
+			"5.2 - Native Sparse Matrices" => "sm_implementation.md",
+			"5.3 - Graph Blas" => "gb_implementation.md",
+		],
+		"Examples" => [
+			"6.1 - Cube Grids" => "example_1.md",
+			"6.2 - ..." => "example_2.md",
+			"6.3 - ..." => "example_3.md",
+		],
 		"A - About the Authors" => "authors.md",
 		"B - Bibliography" => "bibliography.md"
 	]
