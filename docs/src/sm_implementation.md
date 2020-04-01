7f8b217090e80bdbe91636be2e0a41e2b3ddf38f
eOnofri04 <elia.onofri@mensa.it>
2020-04-01 18:09:38 +0200

    Update err->\epsilon

--- docs/src/sm_implementation.md
+++ docs/src/sm_implementation.md
@@ -1,3 +1,3 @@
 # 5.2 - Native Sparse Matrices implementation
 
-In this section we analyse 
+In this section we analyse the Sparse Matrices based implementation of the ```cellCongruence``` algorithm.
--- src/verticesCongruence.jl
+++ src/verticesCongruence.jl
@@ -1,20 +1,20 @@
 """
-	vertCongruence(V::Lar.Array{Float64,2}; err=1e-6)::Tuple{Lar.Points, Array{Array{Int,1},1}}
+	vertCongruence(V::Lar.Array{Float64,2}; ϵ=1e-6)::Tuple{Lar.Points, Array{Array{Int,1},1}}
 
 Evaluates the Vertex Congruence for 3D-points ``V ∈ ℳ(3,n)``.
 
-The function determines the points of ``V`` closer than ``err`` and
+The function determines the points of ``V`` closer than ``ϵ`` and
 builds a new Vertex Set made of the representative of each point cluster.
 
 The method returns:
  - the new Vertex Set
  - a map that, for every new vertex, identifies the old vertices it is made of
 """
-function vertCongruence(V; err=1e-6)
+function vertCongruence(V; ϵ=1e-6)
 	Vcls    = []
 	let visited = [],  kdtree = NearestNeighbors.KDTree(V)
 		for vidx = 1 : size(V, 2)  if !(vidx in visited)
-			nearvs = NearestNeighbors.inrange(kdtree, V[:, vidx], err)
+			nearvs = NearestNeighbors.inrange(kdtree, V[:, vidx], ϵ)
 			push!(Vcls, nearvs)
 			push!(visited, nearvs...)
 		end  end
